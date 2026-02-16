02be2d5f2356117c55f765153bf7de4cb3b6ecb6
Anthony Bianay <anthony.bianayy44@gmail.com>
2026-02-16 14:22:08 +0100

    switch to next/image for all pokemon images

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.pokemondb.net",
-      },
+      { protocol: "https", hostname: "img.pokemondb.net" },
+      { protocol: "https", hostname: "media.licdn.com" },
+      { protocol: "https", hostname: "static.licdn.com" },
     ],
   },
 };
